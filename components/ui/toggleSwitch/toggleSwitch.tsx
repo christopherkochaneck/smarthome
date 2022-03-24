15804338f388da9c0ca8a3b2453f0a3a7bb13cee
Christopher Kochaneck <chrissiko02@gmail.com>
2022-03-24 20:10:49 +0100

    fix: Remove unnecessary code

--- components/ui/toggleSwitch/toggleSwitch.tsx
+++ components/ui/toggleSwitch/toggleSwitch.tsx
@@ -7,27 +7,19 @@ interface Props {
 }
 
 export const ToggleSwitch: FC<Props> = (props) => {
-	useEffect(() => {
-		props.setState(props.state);
-	}, [props.state]);
-
 	return (
-		<div onClick={(e) => e.stopPropagation()}>
-			<Switch
-				checked={props.state}
-				onChange={() => {
-					props.state ? props.setState(!props.state) : props.setState(props.state);
-				}}
+		<Switch
+			checked={props.state}
+			onChange={() => {}}
+			className={`${
+				props.state ? 'bg-zinc-900' : 'bg-zinc-700'
+			} relative inline-flex items-center h-6 rounded-full w-11`}
+		>
+			<span
 				className={`${
-					props.state ? 'bg-zinc-900' : 'bg-zinc-700'
-				} relative inline-flex items-center h-6 rounded-full w-11`}
-			>
-				<span
-					className={`${
-						props.state ? 'translate-x-6' : 'translate-x-1'
-					} inline-block w-4 h-4 transform bg-zinc-500 rounded-full`}
-				/>
-			</Switch>
-		</div>
+					props.state ? 'translate-x-6' : 'translate-x-1'
+				} inline-block w-4 h-4 transform bg-zinc-500 rounded-full`}
+			/>
+		</Switch>
 	);
 };
