e314ed3728d17c2e7acc3c0bf336735522b96c87
Christopher Kochaneck <chrissiko02@gmail.com>
2022-03-25 21:03:17 +0100

    reinstalled node_modules

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
 	images: {
 		domains: ['lh3.googleusercontent.com'],
 	},
-	webpack(config) {
+	webpack: (config) => {
 		config.module.rules.push({
 			test: /\.svg$/,
 			use: ['@svgr/webpack'],
@@ -12,7 +12,6 @@ const nextConfig = {
 		return config;
 	},
 	reactStrictMode: true,
-	env: { OPEN_WEATHER_MAP_API_KEY: process.env.OPEN_WEATHER_MAP_API_KEY },
 };
 
 module.exports = nextConfig;
