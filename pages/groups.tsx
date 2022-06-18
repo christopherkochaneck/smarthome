2cf8acc70575f7c821df4486eafe44b5b695a028
Christopher Kochaneck <chrissiko02@gmail.com>
2022-06-18 17:35:57 +0200

    fix: Groups are now rendered dynamically

--- pages/groups.tsx
+++ pages/groups.tsx
@@ -1,13 +1,17 @@
 import type { NextPage } from 'next';
 import { GroupLightCard } from '../components/groups/GroupLightCard';
 import { LayoutWrapper } from '../components/layout/layoutWrapper';
+import { useGroups } from '../context/GroupContext';
 
 const Groups: NextPage = () => {
+	const groups = useGroups();
 	return (
 		<LayoutWrapper showAppbar={true} showAppbarIcon={true} appBarTitle="Groups" href="/addGroup">
 			<div className="grid gap-4">
 				<div className="text-zinc-500 text-center">Groups</div>
-				<GroupLightCard group="test" title="LED Stripes" />
+				{groups.groups.map((key) => {
+					return <GroupLightCard group={key.name} title={key.name} />;
+				})}
 				<div className="text-zinc-500 text-center">Scenes</div>
 			</div>
 		</LayoutWrapper>
