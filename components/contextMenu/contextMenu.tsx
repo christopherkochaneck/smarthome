dfde76955a3edea8164e101633a34fb57fc6e39e
Christopher Kochaneck <chrissiko02@gmail.com>
2022-06-30 22:43:24 +0200

    Implemented Animation for ContextMenu

--- components/contextMenu/contextMenu.tsx
+++ components/contextMenu/contextMenu.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { createRef, FC, useEffect, useState } from 'react';
 
 interface Props {
 	visible: boolean;
@@ -6,11 +6,25 @@ interface Props {
 }
 
 export const ContextMenu: FC<Props> = (props) => {
+	const ref = createRef<HTMLDivElement>();
+	const [bottom, setBottom] = useState<number>(-1000);
+
+	useEffect(() => {
+		if (!props.visible) {
+			if (ref.current !== null) {
+				setBottom(-ref.current.clientHeight);
+			}
+		} else {
+			setBottom(0);
+		}
+	}, [props.visible]);
+
 	return (
 		<>
 			<div
-				className="w-full h-max bg-black absolute bottom-0 z-99 rounded-t-3xl flex flex-col gap-3 p-4"
-				style={{ display: props.visible ? 'flex' : 'none' }}
+				ref={ref}
+				className="w-full h-max bg-black fixed bottom-0 z-99 rounded-t-3xl flex flex-col gap-3 p-4"
+				style={{ bottom: bottom, transition: 'bottom ease 0.15s' }}
 			>
 				{props.children}
 			</div>
