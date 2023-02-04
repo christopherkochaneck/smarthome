e06b1145902c500530b1bc1eda32b65f46eea22a
Christopher Kochaneck <chrissiko02@gmail.com>
2023-02-04 13:26:18 +0100

    Modified Divider color

--- client/components/ui/list/components/divider/Divider.tsx
+++ client/components/ui/list/components/divider/Divider.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
 
 export const Divider: FC = () => {
-	return <div className="border border-white w-full" />;
+	return <div className="border border-grey w-full" />;
 };
