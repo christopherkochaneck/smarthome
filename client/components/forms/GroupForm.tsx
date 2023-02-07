ab246750a48da7efad81ef09909daad72cf80ce2
Christopher Kochaneck <chrissiko02@gmail.com>
2023-02-08 00:21:31 +0100

    fix(Groupform): Fixed a bug where the ui and ids were not updating onClick

--- client/components/forms/GroupForm.tsx
+++ client/components/forms/GroupForm.tsx
@@ -1,12 +1,14 @@
 import { useRouter } from 'next/router';
-import { FC, FormEvent, useState } from 'react';
+import { FC, FormEvent, useEffect, useState } from 'react';
 import { useGroups } from '../../context/GroupContext';
 import { GroupType } from '../../types/GroupType';
 import { FloatingActionButton } from '../ui/floatingActionButton/floatingActionButton';
 import { Input } from '../ui/input/input';
-import { DeviceFloppy } from 'tabler-icons-react';
+import { DeviceFloppy, IdBadge } from 'tabler-icons-react';
 import { Rgbw2Devices } from '../util/rgbw2devices';
 import { PlugSDevices } from '../util/plugSdevices';
+import { v4 } from 'uuid';
+import { Device } from '../../types/Device';
 
 export const GroupForm: FC = () => {
 	const { addGroup } = useGroups();
@@ -17,7 +19,7 @@ export const GroupForm: FC = () => {
 	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		let group: GroupType = {
-			_id: '',
+			_id: v4(),
 			name: groupName,
 			ids: ids,
 		};
@@ -26,14 +28,10 @@ export const GroupForm: FC = () => {
 		router.push('/groups');
 	};
 
-	const handleAddDevice = (key: any) => {
-		let idArray = ids;
-		if (ids.find((x) => x === key._id)) {
-			idArray.splice(ids.indexOf(key._id!), 1);
-		} else {
-			idArray.push(key._id!);
-		}
-		setIds(idArray);
+	const handleAddDevice = (key: Device) => {
+		if (key._id === undefined) return;
+		const idExists = ids.includes(key._id);
+		setIds(idExists ? ids.filter((x) => x !== key._id) : [...ids, key._id]);
 	};
 
 	return (
