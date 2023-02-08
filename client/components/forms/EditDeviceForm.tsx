fbc4dfebbc5a086e00d896d557bf429baaadef2f
Christopher Kochaneck <chrissiko02@gmail.com>
2023-02-08 08:11:08 +0100

    feat(EditDeviceForm): Implemented the switching of a device type

--- client/components/forms/EditDeviceForm.tsx
+++ client/components/forms/EditDeviceForm.tsx
@@ -4,10 +4,12 @@ import { Input } from '../ui/input/input';
 import { useDevices } from '../../context/DeviceContext';
 import { useRouter } from 'next/router';
 import { DeviceFloppy } from 'tabler-icons-react';
+import { Select } from '../ui/select/select';
 
 export const EditDeviceForm: FC = () => {
-	const { devices, updateDevice } = useDevices();
-	const [deviceType, setDeviceType] = useState<string>('');
+	const { devices, updateDevice, addDevice, deleteDevice } = useDevices();
+	const [initialDeviceType, setInititalDeviceType] = useState<string>('');
+	const [selectedDeviceType, setDeviceType] = useState<string>('');
 	const [deviceName, setDeviceName] = useState<string>('');
 	const [deviceIP, setDeviceIP] = useState<string>('');
 	const router = useRouter();
@@ -18,12 +20,18 @@ export const EditDeviceForm: FC = () => {
 		try {
 			let device: any;
 			device = {
-				type: deviceType,
+				type: initialDeviceType,
 				_id: deviceId,
 				title: deviceName,
 				ipAdress: deviceIP,
 			};
-			return updateDevice(device);
+			if (initialDeviceType === selectedDeviceType) {
+				return updateDevice(device);
+			} else {
+				deleteDevice(device);
+				device.type = selectedDeviceType;
+				addDevice(device);
+			}
 		} catch (ex) {
 			console.log(ex);
 		} finally {
@@ -46,7 +54,7 @@ export const EditDeviceForm: FC = () => {
 			return;
 		}
 
-		setDeviceType(foundID.type);
+		setInititalDeviceType(foundID.type);
 		setDeviceName(foundID.title);
 		setDeviceIP(foundID.ipAdress);
 	}, [router.query, devices]);
@@ -55,6 +63,15 @@ export const EditDeviceForm: FC = () => {
 		<>
 			<form onSubmit={handleSubmit}>
 				<div className="grid gap-4">
+					<Select
+						title="Device Type"
+						values={['RGBW2', 'PlugS', 'HT']}
+						className="h-10 rounded-xl"
+						onChange={(e) => {
+							setDeviceType(e.currentTarget.value);
+						}}
+						value={selectedDeviceType}
+					/>
 					<Input
 						title="Device Name"
 						className="h-10 rounded-xl"
