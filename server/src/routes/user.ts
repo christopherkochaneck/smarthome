eb88950dc86a35a06b9a956390c815d1a43a3094
Christopher Kochaneck <chrissiko02@gmail.com>
2023-02-20 02:07:36 +0100

    Implemented a User endpoint to manage User data

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { User } from '../../models/user';
+import bcrypt from 'bcrypt';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
@@ -9,4 +10,52 @@ router.get('/', async (req, res) => {
   } catch (error) {}
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    return res.status(200).send(user);
+  } catch (error) {}
+});
+
+router.post('/', async (req, res) => {
+  try {
+    let user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    const salt = await bcrypt.genSalt(12);
+    user.password = await bcrypt.hash(user.password, salt);
+
+    user = await user.save();
+
+    console.log(user);
+    res.status(200).send(user);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.patch('/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await User.findById(id);
+
+    if (!user) return res.status(404).send('User not found');
+
+    const salt = await bcrypt.genSalt(12);
+    user.password = await bcrypt.hash(user.password, salt);
+
+    await user.save();
+    return res.status(200).send(user);
+  } catch (error) {}
+});
+
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await User.findByIdAndRemove(id);
+    return res.status(200).send(user);
+  } catch (error) {}
+});
 export default router;
