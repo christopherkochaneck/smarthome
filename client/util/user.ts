ae6f1fa38f1243fb8bc39dc8978d1f23b20cac5b
Christopher Kochaneck <chrissiko02@gmail.com>
2023-02-22 01:44:11 +0100

    feat(User): Added a delete and changePermission method

--- client/util/user.ts
+++ client/util/user.ts
@@ -6,3 +6,15 @@ export const getUsers = async () => {
 	const res = await axios({ method: 'get', url: `${BASE_URL}/api/user` });
 	return res.data;
 };
+
+export const changeUserPermission = async (userId: string, permission: string) => {
+	try {
+		const res = await axios.patch(`${BASE_URL}/api/user/${userId}`, { permission: permission });
+	} catch (error) {}
+};
+
+export const deleteUser = async (userId: string) => {
+	try {
+		const res = await axios.delete(`${BASE_URL}/api/user/${userId}`);
+	} catch (error) {}
+};
