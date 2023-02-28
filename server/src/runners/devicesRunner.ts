14ef4069dc274bbd7fbb812d6c64c33c5d4f6c2a
Christopher Kochaneck <chrissiko02@gmail.com>
2023-02-28 08:30:04 +0100

    feat(socketio): Extended devicesRunner so it can get data for ht, plug and rgbw2

--- server/src/runners/devicesRunner.ts
+++ server/src/runners/devicesRunner.ts
@@ -1,33 +1,50 @@
-import logger from 'tw-logger';
-import color from '../../../client/interfaces/color';
-import { RGBW2 as RGBW2Model } from '../../models/rgbw2';
+import { Device as DeviceModel } from '../../models/device';
 import { socketServer } from '../startup/socketServer';
 import { RGBW2 } from './../../../client/devices/rgbw2';
+import { PlugS } from './../../../client/devices/plugs';
 
 export const dataServiceRunner = () => {
   const devicedata: any[] = [];
-  let i = 0;
   try {
     setInterval(async () => {
-      const devices = await RGBW2Model.find();
-      devices.forEach(async ({ _id, ipAdress }) => {
+      const devices = await DeviceModel.find();
+
+      for (const device of devices) {
+        const { _id, ipAdress, type } = device;
         const id = _id.toString();
-        const d = new RGBW2(ipAdress!, id);
-        const res = await d.fetchCurrentDeviceData();
-        if (res === undefined) return;
+
+        let deviceInstance;
+
+        switch (type) {
+          case 'rgbw2':
+            deviceInstance = new RGBW2(ipAdress!, id);
+            break;
+          case 'plugs':
+            deviceInstance = new PlugS(ipAdress!, id);
+            break;
+          default:
+            continue;
+        }
+
+        const res = await deviceInstance.fetchCurrentDeviceData();
+
+        if (res === undefined) {
+          continue;
+        }
+
         const foundDeviceIndex = devicedata.findIndex(
           (x) => x.hostname === res.hostname
         );
-        if (foundDeviceIndex === -1) return devicedata.push(res);
-        if (
-          JSON.stringify(devicedata[foundDeviceIndex]) === JSON.stringify(res)
-        )
-          return;
 
-        // mullitple req, because power and state dont change sychronously
-        devicedata[foundDeviceIndex] = res;
-        socketServer.emit(id, res);
-      });
+        if (foundDeviceIndex === -1) {
+          devicedata.push(res);
+        } else if (
+          JSON.stringify(devicedata[foundDeviceIndex]) !== JSON.stringify(res)
+        ) {
+          devicedata[foundDeviceIndex] = res;
+          socketServer.emit(id, res);
+        }
+      }
     }, 1000);
   } catch (error: any) {
     console.log(error.message);
