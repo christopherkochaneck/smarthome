aed96b6998675d9e244ddf1a920f6ac034ad4f6e
Christopher Kochaneck <chrissiko02@gmail.com>
2023-03-04 15:48:07 +0100

    feat(accountDetail): Implemented permission based rendering, Added UI Feedback on Delete

--- client/pages/account/[_id].tsx
+++ client/pages/account/[_id].tsx
@@ -1,22 +1,41 @@
+import moment from 'moment';
 import { GetServerSideProps, NextPage } from 'next';
 import { getSession, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { Checkbox, Square, User } from 'tabler-icons-react';
 import { LayoutWrapper } from '../../components/layout/layoutWrapper';
 import { Avatar } from '../../components/ui/avatar/avatar';
 import { Card } from '../../components/ui/card/card';
+import { useToast } from '../../context/ToastContext';
 import { redirectByPermission } from '../../util/redirect';
+import { deleteUser, getUserById } from '../../util/user';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	const session = await getSession(ctx);
 
 	const state = redirectByPermission(session);
 	if (state) return state;
-
 	return { props: {} };
 };
 
 export const AccountDetailPage: NextPage = () => {
+	const { addToast } = useToast();
 	const session = useSession();
+	const router = useRouter();
+
+	const [user, setUser] = useState<any>();
+	const userId = router.query._id?.toString();
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			if (!session.data || !userId) return;
+			const res = await getUserById(session.data?.jwt, userId);
+			setUser(res);
+		};
+		fetchUser();
+	}, []);
+
 	return (
 		<LayoutWrapper
 			className="flex flex-col items-center gap-4"
@@ -33,33 +52,50 @@ export const AccountDetailPage: NextPage = () => {
 			<div className="grid grid-cols-2 gap-4 text-white">
 				<Card className="p-2">
 					<div>Username</div>
-					<div>Chris</div>
+					<div>{user?.name}</div>
 				</Card>
 				<Card className="p-2">
 					<div>Date of creation</div>
+					<div>{moment(user?.dayOfCreation).utc().format('DD.MM.YYYY')}</div>
 				</Card>
 				<Card className="flex flex-col p-2 gap-2">
 					<div>Permissions</div>
 					<span className="flex">
-						{session.data?.user.permission === 'admin' ? <Checkbox /> : <Square />}
-						<span className="flex-grow">Admin</span>
+						{user?.permission === 'admin' ? <Checkbox /> : <Square />}
+						<span className="grow">Admin</span>
 					</span>
 					<span className="flex">
-						{session.data?.user.permission === 'user' ? <Checkbox /> : <Square />}
-						<span className="flex-grow">User</span>
+						{user?.permission === 'user' ? <Checkbox /> : <Square />}
+						<span className="grow">User</span>
 					</span>
 					<span className="flex">
-						{session.data?.user.permission === 'unauthorized' ? <Checkbox /> : <Square />}
-						<span className="flex-grow">Unauthorized</span>
+						{user?.permission === 'unauthorized' ? <Checkbox /> : <Square />}
+						<span className="grow">Unauthorized</span>
 					</span>
 					<span className="flex">
-						{session.data?.user.permission === 'denied' ? <Checkbox /> : <Square />}
-						<span className="flex-grow">Denied</span>
+						{user?.permission === 'denied' ? <Checkbox /> : <Square />}
+						<span className="grow">Denied</span>
 					</span>
 				</Card>
 			</div>
-			<button className="p-2 bg-black rounded-xl text-white">Change Permissions</button>
-			<button className="p-2 bg-red-700 rounded-xl text-white">Delete Account</button>
+			<span className="flex flex-col w-full gap-2">
+				<button className="p-2 bg-black rounded-xl text-white w-full">Change Permissions</button>
+				<button
+					className="p-2 bg-red-700 rounded-xl text-white w-full"
+					onClick={async () => {
+						if (!userId || !session.data)
+							return addToast({ message: 'Something went wrong', type: 'error' });
+						const res = await deleteUser(userId, session.data?.jwt);
+						if (res?.status === 200) {
+							router.push('/manageAccounts');
+							return addToast({ message: 'User deleted successfully', type: 'success' });
+						}
+						addToast({ message: 'Something went wrong', type: 'error' });
+					}}
+				>
+					Delete Account
+				</button>
+			</span>
 		</LayoutWrapper>
 	);
 };
